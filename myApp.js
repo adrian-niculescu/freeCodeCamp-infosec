c26c2f7e3a96971bfdae9af447592543dabec00f
Adrian Niculescu <adrian-niculescu@users.noreply.github.com>
2022-06-19 15:52:46 +0000

    Activated 90 days HTTP Strict Transport Security

--- myApp.js
+++ myApp.js
@@ -10,8 +10,11 @@ app.use(helmet.frameguard({
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-
-
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+app.use(helmet.hsts({
+    maxAge: ninetyDaysInSeconds,
+    force: true
+}));
 
 
 
