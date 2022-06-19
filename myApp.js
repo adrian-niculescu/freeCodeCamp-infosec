e507f542402ef3229442cbba5278a83cf4804fef
Adrian Niculescu <adrian-niculescu@users.noreply.github.com>
2022-06-19 15:39:29 +0000

    Disallow usage in iframe.

--- myApp.js
+++ myApp.js
@@ -3,7 +3,9 @@ const helmet = require('helmet');
 const app = express();
 
 app.use(helmet.hidePoweredBy());
-
+app.use(helmet.frameguard({
+    action: "deny",
+}));
 
 
 
@@ -54,10 +56,10 @@ const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function(request, response) {
+    response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+    console.log(`Your app is listening on port ${port}`);
 });
